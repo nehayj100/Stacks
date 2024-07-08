db9751f128b9dc4a77c7acf8f4809dc5be6722ed
Nehayj100 <nehayj100@tamu.edu>
2024-07-08 15:31:15 -0500

    Ex1 done

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,33 @@
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+  def __init__(self):
+    self.s = []
+  
+  def isEmpty(self):
+    return 1 if len(self.s) == 0 else 0
+
+  def push(self, item):
+    self.s.append(item)
+    # so if s = [1,2,3] and item = 4; s= [1,2,3,4]s
+
+  def pop(self):
+    return self.s.pop()
+
+  def peek(self):
+    return s[len(s) - 1]
+
+  def size(self):
+    return len(s)
+
+  def show(self):
+    print(self.s)
 
 s = myStack()
 s.push('1')
 s.push('2')
+s.push('3')
+s.show()
 print(s.pop())
-print(s.show())
+s.show()
+

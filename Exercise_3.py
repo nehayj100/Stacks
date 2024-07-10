00a0d117f83894058b41c6eb3ea14b2449b8d19b
Nehayj100 <nehayj100@tamu.edu>
2024-07-10 17:01:59 -0500

    Ex3 done

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,8 +1,15 @@
+# Time Complexity : O(n)
+# Space Complexity : O(n)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+
 class ListNode:
     """
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = None
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,16 +24,43 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        
+        new_node = ListNode(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            curr = self.head
+            while curr.next is not None:
+                curr = curr.next
+            curr.next = new_node
+                
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        if self.head is None:
+            return None
+        curr = self.head
+        while curr is not None:
+            if curr.data == key:
+                return curr.data
+        return None
+        
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if self.head.data == key:
+            self.head = self.head.next
+        else:
+            prev, curr = None, self.head
+            while curr is not None:
+                prev = curr 
+                curr = curr.next
+                if curr.data == key:
+                    prev.next = curr.next
+                    break
+        
